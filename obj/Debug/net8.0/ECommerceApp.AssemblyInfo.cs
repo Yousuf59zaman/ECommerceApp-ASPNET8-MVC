981df4f0ae16abaa9ad4f02e079602e82e3f02b6
Your Name <Md Yousuf Zaman>
2024-08-26 21:46:11 +0600

    Made migration using ApplicationUser.cs Identity class as I had made migration before using the User.cs class but deleted the User.cs class so i deleted that migrations folder and database table in sql server

--- obj/Debug/net8.0/ECommerceApp.AssemblyInfo.cs
+++ obj/Debug/net8.0/ECommerceApp.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("ECommerceApp")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+4dfb1a33b98dc12edca2594400909f71b5f18df9")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+9f3cb261eb0f457937ee8a160d943182b8c3860b")]
 [assembly: System.Reflection.AssemblyProductAttribute("ECommerceApp")]
 [assembly: System.Reflection.AssemblyTitleAttribute("ECommerceApp")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
