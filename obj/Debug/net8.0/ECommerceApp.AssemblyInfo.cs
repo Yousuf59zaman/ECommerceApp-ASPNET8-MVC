15d7009752a83dc908ec36313fc079dafc7e9a04
Zaman <zaman@bdjobs.local>
2024-08-27 17:43:29 +0600

    Created a new layout (_AuthLayout.cshtml) and updated Register.cshtml and Login.cshtml to use it, removing the default layout for these pages.

--- obj/Debug/net8.0/ECommerceApp.AssemblyInfo.cs
+++ obj/Debug/net8.0/ECommerceApp.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("ECommerceApp")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+05823e287fcccc9a2d06d7bfd6cbcc327de37d9b")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+4983d569b7a1599d6ee7cd79c8e792745911e4d7")]
 [assembly: System.Reflection.AssemblyProductAttribute("ECommerceApp")]
 [assembly: System.Reflection.AssemblyTitleAttribute("ECommerceApp")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
