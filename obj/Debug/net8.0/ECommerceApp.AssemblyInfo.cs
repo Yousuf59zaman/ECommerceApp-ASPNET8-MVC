db5272e7aa0f5709f76261d8eb524fe0e15440e3
Zaman <zaman@bdjobs.local>
2024-08-29 17:23:17 +0600

    Added Track Orders feature to display order history for the logged-in user

--- obj/Debug/net8.0/ECommerceApp.AssemblyInfo.cs
+++ obj/Debug/net8.0/ECommerceApp.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("ECommerceApp")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+5f9abd0659ffae64c20b2e6543ea4d44d08d9cdd")]
 [assembly: System.Reflection.AssemblyProductAttribute("ECommerceApp")]
 [assembly: System.Reflection.AssemblyTitleAttribute("ECommerceApp")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
